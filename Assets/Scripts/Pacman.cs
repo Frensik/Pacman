deb6ee39b2d34e73183428767b52ec8dc05ad04f
LAPTOP-2NG5M1DQ\goode <goode5013@gmail.com>
2025-01-20 06:34:51 +0000

    Added movement to pacman using the new input system Made him rotate to face direction using old input system Added collision to maze Added tilemap for pellets (not sure if that's how it works yet but I'm giving it a go)

---
+++ Assets/Scripts/Pacman.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacman : MonoBehaviour
+{
+
+//The numbers
+    [SerializeField] float moveSpeed = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // This code rotates pacman to face the way he's moving
+        //Script based off of 
+        Moving();
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, -90f);
+        }
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 180f);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+    }
+
+    private void Moving()
+    {
+        //Movement script taken from schmup lecture 1
+        var xInput = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        var newXPos = transform.position.x + xInput;
+
+        var yInput = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        var newYPos = transform.position.y + yInput;
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
